55176bfc8cd63fb1a3127e7ec7547a04179f5987
Aryan Yadav <ay6033756@gmail.com>
2026-06-15 19:30:02 +0530

    update field validation

--- 3.field_validation.py
+++ 3.field_validation.py
@@ -1,27 +1,49 @@
-from pydantic import BaseModel , EmailStr , AnyUrl
+# Field validation is used to data validation in a single field of a model or It is used to validate the data of a single field in a model.
+
+from pydantic import BaseModel , EmailStr , Field , field_validator
 from typing import Optional , List ,Dict
 
 class Patient(BaseModel):
     
     name : str
     email : EmailStr
-    linkedin_url : AnyUrl
-    
     age : int
-    
     weight : float
-    
     married : bool = False 
-    
     allergies : Optional[List[str]] = None  
-    
     contact_details : Dict[str, str] 
+    
+    @field_validator('email')
+    @classmethod
+    def email_validator(cls,value):
+        
+        valid_domains =['hdfc.com','icici.com']
+        
+        domain_name = value.split('@')[-1]
+        
+        if domain_name not in valid_domains:
+            raise ValueError("Not valid domain ")
+        
+        return value
+    
+    
+    @field_validator('name')
+    @classmethod
+    def transform_name(cls, value):
+        return value.upper()
+    
+    
+    @field_validator('age')
+    @classmethod
+    def age_validator(cls, value):
+        if value < 0 or value > 200:
+            raise ValueError("Age must be between 0 and 200")
+        return value
 
 def update_patient_data(patient: Patient ):
     
     print(patient.name)
     print(patient.email)
-    print(patient.linkedin_url)
     print(patient.age)
     print(patient.weight)
     print(patient.married)
@@ -31,8 +53,7 @@ def update_patient_data(patient: Patient ):
     
 Patient_info = {
     "name" : "John Doe",
-    "email" : "john.doe@example.com",
-    "linkedin_url" : "https://www.linkedin.com/in/johndoe",
+    "email" : "john.doe@hdfc.com",
     "age" : 30,
     "weight" : 70.5,
     "married" : True,
